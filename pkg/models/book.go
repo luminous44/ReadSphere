bf241ea61d8d5aec7a5a18668b987c463d5418e4
luminous44 <aklemasultanakhi@gmail.com>
2025-11-08 10:42:55 +0600

    added db required function

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -9,8 +9,7 @@ var db *gorm.DB
 
 type Book struct{
 	gorm.Model
-
-	Name string `gorm:"primaryKey" json:"name"`
+	Name string `gorm:"" json:"name"`
 	Author string `json:"author"`
 	Publication string `json:"publication"`
 }
@@ -19,4 +18,28 @@ func init(){
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB){
+	var getBook Book
+	db := db.Where("ID=?",Id).Find(&getBook)
+	return  &getBook, db
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?",ID).Delete(book)
+	return book
 }
