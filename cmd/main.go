373fb57a3aec3734a723a822e89fc20e1b5fac55
luminous44 <aklemasultanakhi@gmail.com>
2025-11-08 09:31:04 +0600

    added main route

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main()
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/luminous44/ReadSphere/pkg/routes"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010",r))
+}
