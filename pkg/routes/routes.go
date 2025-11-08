3aefc4471c763479dd29c92aff1548f2fbb13803
luminous44 <aklemasultanakhi@gmail.com>
2025-11-08 07:54:41 +0600

    added route

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,2 +1,16 @@
 package routes
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/luminous44/ReadSphere/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router){
+
+	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.GetBooksById).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("//book/{id}", controllers.DeleteBook).Methods("DELETE")
+
+}
